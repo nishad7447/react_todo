feac588a5c8fc37677bd57cf3dcc641b02eeb2b8
nishad7447 <tkottilnishad19@gmail.com>
2023-06-03 19:53:40 +0530

    finised todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 function App() {
   const [todos,setTodos]=useState([])
   const [todo,setTodo]=useState('')
+  console.log(todos)
+
   return (
     <div className="app">
     <div className="mainHeading">
@@ -12,28 +14,52 @@ function App() {
     </div>
     <div className="subHeading">
       <br />
-      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      <h2>
+          {(() => {
+            const currentWeekday = new Date().toLocaleDateString(undefined, {weekday: 'long'});
+            return `Whoop, it's ${currentWeekday} 🌝 ☕`;
+          })()}
+        </h2>
     </div>
     <div className="input">
       <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-      <i onClick={()=>setTodos([...todos,todo])} className="fas fa-plus"></i>
+      <i onClick={()=>setTodos([...todos,{id:Date.now(),text:todo,status:false}])} className="fas fa-plus"></i>
     </div>
     <div className="todos">
      {
-      todos.map((value)=>{
+      todos.map((obj)=>{
         return(<>
          <div className="todo">
         <div className="left">
-          <input type="checkbox" name="" id="" />
-          <p>{value}</p>
+          <input onChange={(e)=>{
+            setTodos(todos.filter(filterObj=>{
+              if(filterObj.id===obj.id){
+                filterObj.status=e.target.checked
+              }
+              return filterObj.id
+            }))
+            }} value={obj.status} type="checkbox" name="" id="" />
+          <p className={obj.status ? "crossed-over" : ""}>{obj.text}</p>
         </div>
         <div className="right">
-          <i className="fas fa-times"></i>
+          <i onClick={()=>{
+            setTodos(todos.filter((filterObj)=>filterObj.id!== obj.id))
+            }} className="fas fa-times"></i>
         </div>
       </div>
         </>)
       })
      }
+
+     {
+      todos.map((obj)=>{
+        if(obj.status){
+          return(<h1 className='finished'>{obj.text}</h1>)
+        }
+        return null
+      })
+     }
+
     </div>
   </div>
   );
